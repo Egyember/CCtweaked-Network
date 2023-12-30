0661c6ad68713f3fa1f0bf89b792d561d9747c12
Egy_ember <kamufiok06@gmail.com>
2023-12-30 18:45:00 +0100

    updated download script

--- helper/download.lua
+++ helper/download.lua
@@ -1,4 +1,4 @@
-shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/master/src/base.lua")
-shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/master/src/stack.lua")
-shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/master/src/startup.lua")
+shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/battery_controller/src/base.lua")
+shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/battery_controller/src/stack.lua")
+shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/battery_controller/src/startup.lua")
 fs.makeDir("/do")
