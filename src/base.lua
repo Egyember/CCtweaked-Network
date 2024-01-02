6c566d36b5102f228d25ed5544306be0cc5847a2
Egy_ember <kamufiok06@gmail.com>
2024-01-02 18:56:47 +0100

    after rebase fix

--- src/base.lua
+++ src/base.lua
@@ -6,8 +6,6 @@ port = 41
 osloop = 1
 suportedREQs = "ECHO,SUPR,SUPD,DOIN,BATS"
 suportedDOs = ""
-fueldir = "down"
-numberOfBatterys = 6 --[0:9]
 
 --global data
 charge = 0
@@ -119,7 +117,6 @@ function mkReq(msgID, msgType, msgBody)
 	return msgID .. msgType .. msgBody
 end
 
---todo: charge, maxcharge
 function request(msg,  senderID)
 --handle requests (general header striped)	
 	local msgID, msgType, msgBody = extractRequestHeader(msg)
@@ -215,56 +212,6 @@ function lisenNet()
 	end
 end
 
-function batteryupdate()
-	function getBatteryData()
-		local perNames = peripheral.getNames()
-		local cap = 0
-		local maxcap = 0
-		for i = 1, #perNames do
-			local type1, type2 = peripheral.getType(perNames[i])
-			if  type2 == "energy_storage" then
-				cap = cap + peripheral.call(perNames[i], "getEnergy")
-				maxcap = maxcap + peripheral.call(perNames[i], "getEnergyCapacity")
-			end
-		end
-		return cap , maxcap
-	end
-
-	function refuel()
-		if fueldir == "down" then
-			turtle.suckDown()
-		elseif fueldir == "up" then
-			turtle.suckUp()
-		end
-		turtle.refuel()
-	end
-
-	function sampleEnergy()
-		if turtle.getFuelLevel() < 1000 then
-			refuel()
-		end
-		local capSum = 0
-		local maxcapSum = 0
-		local lastsuc
-		for i = 1, numberOfBatterys do
-			local cap , maxcap = getBatteryData()
-			capSum = capSum + cap
-			maxcapSum = maxcapSum + maxcap
-			turtle.forward()
-			lastsuc = turtle.forward()
-		end
-		--returning home
-		local homeDist = numberOfBatterys *2
-		if not lastsuc then
-			homeDist = homeDist-1
-		end
-		for i = 1, homeDist do
-			turtle.back()
-		end
-		return capSum, maxcapSum
-	end
-	charge, maxCharge = sampleEnergy()
-end
 function doingTasks()
 	while true do
 		local task = doStack.pop()
