96c88b838619c560699946bb87da377847f41bf6
Egy_ember <kamufiok06@gmail.com>
2024-01-29 22:53:24 +0100

    added file

---
+++ src/globalVariable.lua
@@ -0,0 +1,9 @@
+--constants
+ID = "base" --must be 4 char
+switching = false
+switchingBlacklist = "" --IDs not to switch (to avoid switching loops mosty cased by ender modem and wireless modem)
+port = 41
+osloop = 1
+suportedREQs = "ECHO,SUPR,SUPD"
+suportedDOs = ""
+
