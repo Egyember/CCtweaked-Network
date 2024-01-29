e0080a4c0e316ab5838aeb44918365aa83ae5e85
Egyember <79502228+Egyember@users.noreply.github.com>
2024-01-29 22:59:53 +0100

    Update download.lua

--- helper/download.lua
+++ helper/download.lua
@@ -1,4 +1,5 @@
 shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/master/src/base.lua")
 shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/master/src/stack.lua")
 shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/master/src/startup.lua")
+shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/master/src/globalVariable.lua")
 fs.makeDir("/do")
