e171547a9eac8fabf26bac488bd66c0935a30081
Egyember <79502228+Egyember@users.noreply.github.com>
2024-01-29 18:52:30 +0100

    Update download.lua

--- helper/download.lua
+++ helper/download.lua
@@ -4,3 +4,5 @@ shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/turtle(battery
 fs.makeDir("/do")
 shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/turtle(battery)/src/do/batteryUpdate.lua")
 shell.run("mv batteryupdate.lua /do/")
+shell.run("wget https://github.com/Egyember/CCtweaked-Network/raw/turtle(battery)/src/do/returnHome.lua")
+shell.run("mv returnHome.lua /do/")
