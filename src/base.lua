f3250471e441b26d2620ea1baa9c980d95e99e94
Egy_ember <kamufiok06@gmail.com>
2024-01-29 22:34:58 +0100

    separetad globals for a hack

--- src/base.lua
+++ src/base.lua
@@ -1,12 +1,3 @@
---constants
-ID = "base" --must be 4 char
-switching = false
-switchingBlacklist = "" --IDs not to switch (to avoid switching loops mosty cased by ender modem and wireless modem)
-port = 41
-osloop = 1
-suportedREQs = "ECHO,SUPR,SUPD"
-suportedDOs = ""
-
 --loading libs
 dofile "stack.lua"
 
